afce08726055743d4d9ee296a07ca8316d2f255c
david1707 <DavidAlacant@gmail.com>
2024-01-07 15:31:16 +0100

    Testing with Jest added

---
+++ server.test.js
@@ -0,0 +1,9 @@
+// Mock tests added to check if Jenkins can handle them
+
+describe("Fake tests", () => {
+  test("adding 1 + 2 should return 3", () => {
+    var result = 1 + 2;
+
+    expect(result).toBe(3);
+  });
+});
